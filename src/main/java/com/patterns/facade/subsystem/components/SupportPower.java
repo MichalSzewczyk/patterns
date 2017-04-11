1d07426692f74a24eda45e3564882d59ccdb84be
mszewczyk <szewczykm94@gmail.com>
2017-04-11 13:29:19 +0200

    Created model for facade example

---
+++ src/main/java/com/patterns/facade/subsystem/components/SupportPower.java
@@ -0,0 +1,12 @@
+package com.patterns.facade.subsystem.components;
+
+/**
+ * Created by Michał Szewczyk on 2017-04-11.
+ */
+public class SupportPower {
+    private boolean turnedOn;
+
+    public void setTurnedOn(boolean turnedOn) {
+        this.turnedOn = turnedOn;
+    }
+}
