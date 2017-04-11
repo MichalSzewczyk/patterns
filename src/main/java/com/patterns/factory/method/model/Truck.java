5fd2e7371acadd5d3ea447197515652b5cb17ca4
mszewczyk <szewczykm94@gmail.com>
2017-04-11 08:12:08 +0200

    Basic model to present pattern

---
+++ src/main/java/com/patterns/factory/method/model/Truck.java
@@ -0,0 +1,10 @@
+package com.patterns.factory.method.model;
+
+/**
+ * Created by Michał Szewczyk on 2017-04-11.
+ */
+public class Truck implements Vehicle {
+    public void drive() {
+        System.out.println("Driving Truck");
+    }
+}
---
+++ src/main/java/com/patterns/factory/method/model/Vehicle.java
@@ -0,0 +1,8 @@
+package com.patterns.factory.method.model;
+
+/**
+ * Created by Michał Szewczyk on 2017-04-11.
+ */
+public interface Vehicle {
+    void drive();
+}
