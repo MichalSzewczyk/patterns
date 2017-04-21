4f531d811fd57099c00f9045d40741a3ba77d008
mszewczyk <szewczykm94@gmail.com>
2017-04-21 09:35:57 +0200

    Created swing application

---
+++ src/main/java/com/patterns/command/SwingApp.java
@@ -0,0 +1,19 @@
+package com.patterns.command;
+
+
+import com.patterns.command.swing.interfaces.GuiGenerator;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Created by Michał Szewczyk on 2017-04-20.
+ * <p>
+ * Basic swing application to show usage of command pattern.
+ */
+public class SwingApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.patterns.command");
+        GuiGenerator generator = context.getBean(GuiGenerator.class);
+
+        javax.swing.SwingUtilities.invokeLater(generator::generateGui);
+    }
+}
