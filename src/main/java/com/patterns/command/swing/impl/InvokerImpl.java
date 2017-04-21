93271bd28502b6cdd068f04a4d35d7329d842a47
mszewczyk <szewczykm94@gmail.com>
2017-04-21 09:34:39 +0200

    Created invoker implementation

---
+++ src/main/java/com/patterns/command/swing/impl/InvokerImpl.java
@@ -0,0 +1,48 @@
+package com.patterns.command.swing.impl;
+
+import com.patterns.command.swing.interfaces.Invoker;
+import com.patterns.command.swing.util.ActionLinkedList;
+import com.patterns.command.swing.util.ActionList;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Michał Szewczyk on 2017-04-21.
+ */
+
+@Service
+public class InvokerImpl implements Invoker<KeyEventCommand> {
+    private final ActionList<KeyEventCommand> commands;
+
+    public InvokerImpl() {
+        this.commands = new ActionLinkedList<>();
+    }
+
+    @Override
+    public void storeCommand(KeyEventCommand textCommand) {
+        this.commands.add(textCommand);
+    }
+
+    @Override
+    public boolean canUndo() {
+        return this.commands.canStepBack();
+    }
+
+    @Override
+    public boolean canRedo() {
+        return this.commands.canStepForward();
+    }
+
+    @Override
+    public void undo() {
+        KeyEventCommand command = commands.stepBack();
+        command.undo();
+    }
+
+    @Override
+    public void redo() {
+        KeyEventCommand command = commands.stepForward();
+        command.execute();
+    }
+
+
+}
