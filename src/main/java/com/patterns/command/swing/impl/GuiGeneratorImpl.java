52deb77e17a59bc5b72f54677237e65e705e2ae2
mszewczyk <szewczykm94@gmail.com>
2017-04-21 09:35:08 +0200

    Created gui generator

---
+++ src/main/java/com/patterns/command/swing/impl/GuiGeneratorImpl.java
@@ -0,0 +1,101 @@
+package com.patterns.command.swing.impl;
+
+import com.patterns.command.swing.interfaces.GuiGenerator;
+import com.patterns.command.swing.interfaces.Invoker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Created by Michał Szewczyk on 2017-04-20.
+ */
+
+@Component
+public class GuiGeneratorImpl implements GuiGenerator {
+    private JFrame frame;
+    private JTextField textField;
+    private JButton undoButton;
+    private JButton redoButton;
+    private Invoker<KeyEventCommand> invoker;
+
+    @Autowired
+    public GuiGeneratorImpl(Invoker<KeyEventCommand> invoker) {
+        this.invoker = invoker;
+        invoker.storeCommand(new KeyEventCommand(textField, "", ""));
+    }
+
+    private void prepareMainFrame() {
+        frame = new JFrame("Basic Text Editor");
+        frame.setLayout(new GridLayout(3, 1));
+        frame.setPreferredSize(new Dimension(400, 400));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private void prepareTextField() {
+        textField = new JTextField("");
+    }
+
+    private void prepareButtons() {
+        undoButton = new JButton("Undo");
+        undoButton.setEnabled(false);
+        redoButton = new JButton("Redo");
+        redoButton.setEnabled(false);
+    }
+
+    private void combineElements() {
+        frame.getContentPane().add(textField);
+        frame.getContentPane().add(undoButton);
+        frame.getContentPane().add(redoButton);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void refreshButtons() {
+        redoButton.setEnabled(invoker.canRedo());
+        undoButton.setEnabled(invoker.canUndo());
+    }
+
+    private void setListeners() {
+        undoButton.addActionListener((actionEvent) -> {
+            invoker.undo();
+            refreshButtons();
+        });
+        redoButton.addActionListener((actionEvent) -> {
+            invoker.redo();
+            refreshButtons();
+        });
+
+        textField.addKeyListener(new KeyListener() {
+            private String before;
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                before = textField.getText();
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                invoker.storeCommand(new KeyEventCommand(textField, textField.getText(), before));
+                refreshButtons();
+            }
+        });
+    }
+
+    @Override
+    public void generateGui() {
+        prepareButtons();
+        prepareMainFrame();
+        prepareTextField();
+        setListeners();
+
+        combineElements();
+    }
+}
