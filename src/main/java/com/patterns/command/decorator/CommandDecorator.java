f6c1ef95eb7216985c35bfba5fbb94d3b7082d61
mszewczyk <szewczykm94@gmail.com>
2017-04-21 10:25:11 +0200

    Added decorator to count method invocations

---
+++ src/main/java/com/patterns/command/decorator/CommandDecorator.java
@@ -0,0 +1,31 @@
+package com.patterns.command.decorator;
+
+import com.patterns.command.interfaces.Command;
+
+/**
+ * Created by Michał Szewczyk on 2017-04-21.
+ */
+public class CommandDecorator implements Command {
+    private final Command command;
+    private static int counter;
+
+    public CommandDecorator(Command command) {
+        this.command = command;
+    }
+
+    @Override
+    public void execute() {
+        command.execute();
+        counter++;
+    }
+
+    @Override
+    public void undo() {
+        command.undo();
+        counter--;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+}
