98d08b7f297b452ace20b79513a886ebef3feb87
mszewczyk <szewczykm94@gmail.com>
2017-04-29 21:55:55 +0200

    Created abstract observable

---
+++ src/main/java/com/patterns/observer/impl/AbstractObservable.java
@@ -0,0 +1,23 @@
+package com.patterns.observer.impl;
+
+import com.patterns.observer.interfaces.Observable;
+import com.patterns.observer.interfaces.Observer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Michał Szewczyk on 2017-04-29.
+ */
+public abstract class AbstractObservable implements Observable {
+    protected List<Observer> observers;
+
+    public AbstractObservable() {
+        this.observers = new LinkedList<>();
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        this.observers.add(observer);
+    }
+}
